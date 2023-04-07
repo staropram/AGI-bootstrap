ea4ab261aed8e4dd6ac469b916d0a4d30c1fc5b0
Ashley Mills <ashley@ashleymills.com>
2023-04-07 22:35:03 +0100

    added a show_command_source command

---
+++ commands/show_command_source.R
@@ -0,0 +1,17 @@
+command_show_command_source <- list(
+	usage='{"action":"show_command_source","command_name":"name"} will show the R source for the specified command "name".',
+	f=function(args) {
+		fn <- paste0("commands/",args$command_name,'.R')
+		if(!file.exists(fn)) {
+			return('{"error":"Invalid command"}')
+		}
+		f <- file(fn,"r")
+		content <- paste(readLines(f),collapse="\n")
+		close(f)
+		content
+	}
+)
+
+#args <- list()
+#args$command_name <- "source_r_file"
+#print(command_show_command_source$f(args))
