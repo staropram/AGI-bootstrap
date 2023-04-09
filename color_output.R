2a7c7fad24c8650364bcd7aecfa03cc16563b869
Ashley Mills <ashley@ashleymills.com>
2023-04-09 22:30:04 +0100

    abstracted color terminal output

---
+++ color_output.R
@@ -0,0 +1,25 @@
+get_ansi_color <- function(color_number) {
+  sprintf("\033[38;5;%dm", color_number)
+}
+
+# Define ANSI escape codes for colors
+commentColor <- get_ansi_color(93)
+paramColor <- get_ansi_color(42)
+actionColor <- get_ansi_color(214)
+resetColor <- "\033[0m"
+
+print_action <- function(action) {
+	cat(actionColor,"action: \"",action,"\"",resetColor,"\n",sep="")
+}
+
+print_comment <- function(comment) {
+	cat(commentColor,"comment: \"",comment,"\"",resetColor,"\n",sep="")
+}
+
+print_param <- function(paramName,paramValue) {
+	cat(paramColor,paramName,": \"",paramValue,"\"",resetColor,"\n",sep="")
+}
+
+#print_action("test_action")
+#print_comment("test_comment")
+#print_param("test_param")
